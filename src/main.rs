4072320f57a6778514a8e38947799c0fd3f7d9e7
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2026-01-23 16:08:09 +0200

    feat(game): add lighting system and entity spawning with interaction logic
    Add directional and point light spawning, implement person entities with names,
    create greeting and updating systems for people, and chain update operations

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,49 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, (spawn_camera, spawn_light))
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
         .add_plugins(DefaultPlugins)
         .run();
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera3d::default());
+}
+
+fn spawn_light(mut commands: Commands) {
     commands.spawn(DirectionalLight::default());
     commands.spawn(PointLight::default());
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break;
+        }
+    }
 }
